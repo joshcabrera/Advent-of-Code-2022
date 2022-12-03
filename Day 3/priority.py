c0317509087e0a61f381baf006eb65f4e84f62b1
Josh Cabrera <cabrera.josh@gmail.com>
2022-12-03 15:50:49 -0500

    day 3

---
+++ Day 3/priority.py
@@ -0,0 +1,78 @@
+priority = {
+    "a":1,
+    "b":2,
+    "c":3,
+    "d":4,
+    "e":5,
+    "f":6,
+    "g":7,
+    "h":8,
+    "i":9,
+    "j":10,
+    "k":11,
+    "l":12,
+    "m":13,
+    "n":14,
+    "o":15,
+    "p":16,
+    "q":17,
+    "r":18,
+    "s":19,
+    "t":20,
+    "u":21,
+    "v":22,
+    "w":23,
+    "x":24,
+    "y":25,
+    "z":26,
+    "A":27,
+    "B":28,
+    "C":29,
+    "D":30,
+    "E":31,
+    "F":32,
+    "G":33,
+    "H":34,
+    "I":35,
+    "J":36,
+    "K":37,
+    "L":38,
+    "M":39,
+    "N":40,
+    "O":41,
+    "P":42,
+    "Q":43,
+    "R":44,
+    "S":45,
+    "T":46,
+    "U":47,
+    "V":48,
+    "W":49,
+    "X":50,
+    "Y":51,
+    "Z":52
+}
+
+sum_ = 0
+
+f = open("input.txt")
+
+def splitString(string):
+    first_half = string[0:len(string)//2]
+    second_half = string[len(string)//2:]
+    return [first_half,second_half]
+
+
+for x in f:
+    common = ""
+    rucksack = splitString(x)
+    for i in rucksack[0]:
+        for j in rucksack [1]:
+            if i == j:
+                common = i
+    sum_ += priority.get(common)          
+
+print (sum_)
+
+
+
